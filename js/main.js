c1afbeec728956cda9d607c38869420d7d8e9b36
Anti614 <julianikulina55@gmail.com>
2021-09-13 17:43:35 -0400

    refactor assets

--- js/main.js
+++ js/main.js
@@ -1,26 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   let nav = $("#navbarMenu");
-  $(".navbar a").click(function() {
-        
-      $("body,html").animate({
-       scrollTop:$("#" + $(this).data('value')).offset().top - $("#header").height()
-      }, 700);
-      nav.removeClass("show");
+  $(".navbar a").click(function () {
+
+    $("body,html").animate({
+      scrollTop: $("#" + $(this).data('value')).offset().top - $("#header").height()
+    }, 700);
+    nav.removeClass("show");
 
   });
 
 
-  $('#toggle-search').click(function(){
+  $('#toggle-search').click(function () {
     $('#toggle-search').toggleClass('active');
-    $('#searchBar').toggle(250);
-    });
+    $('#searchBar').toggle(500);
+  });
+
+  $('body').click(function (e) {
+
+    if (e.target.id !== 'searchBar' && e.target.parentElement.id !== 'toggle-search') {
+      $('#toggle-search').removeClass('active');
+      $('#searchBar').fadeOut(1000);
+
+    }
+  });
 
 
 
 
   $('#carouselGallery').carousel({
     interval: false
-    });
-    
+  });
+
+
 });
+
